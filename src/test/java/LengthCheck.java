68e0dc9d4f9abeb9e5f306cd201cbb804b6cf964
macle <ysys86a@gmail.com>
2019-11-06 23:05:38 +0900

    테스트 예제 추가

--- src/test/java/LengthCheck.java
+++
@@ -1,6 +0,0 @@
-public class LengthCheck {
-    public static void main(String[] args) {
-
-        System.out.println("TB_SYSTEM_SERVER_CONFIG".length());
-    }
-}
--- src/test/java/PingTest.java
+++ src/test/java/PingTest.java
@@ -1,9 +1,9 @@
 import com.seomse.system.engine.console.EngineConsole;
-
+import com.seomse.system.server.console.ServerConsole;
 /**
  * <pre>
- *  파 일 명 : EngineStart.java
- *  설    명 : 엔진시작 테스트
+ *  파 일 명 : PingTest.java
+ *  설    명 : 핑테스트
  *  작 성 자 : macle
  *  작 성 일 : 2019.11.06
  *  버    전 : 1.0
@@ -12,16 +12,10 @@ import com.seomse.system.engine.console.EngineConsole;
  * </pre>
  * @author Copyrights 2019 by ㈜섬세한사람들. All right reserved.
  */
-public class EngineStart {
-
+public class PingTest {
     public static void main(String[] args) {
-
-
-
-        System.out.println(EngineConsole.engineStart("E58"));
-
-
-
+        System.out.println(EngineConsole.ping("E58"));
+//        System.out.println(ServerConsole.ping("S58"));
 
     }
 }
