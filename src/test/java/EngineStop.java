330996c925c5559d8e1f2276ffc569a139c391ce
macle <ysys86a@gmail.com>
2019-11-06 23:07:16 +0900

    설치 테스트 소스 추가

---
+++ src/test/java/EngineStop.java
@@ -0,0 +1,20 @@
+import com.seomse.system.engine.console.EngineConsole;
+/**
+ * <pre>
+ *  파 일 명 : EngineStart.java
+ *  설    명 : 엔진중지 테스트
+ *  작 성 자 : macle
+ *  작 성 일 : 2019.11.06
+ *  버    전 : 1.0
+ *  수정이력 :
+ *  기타사항 :
+ * </pre>
+ * @author Copyrights 2019 by ㈜섬세한사람들. All right reserved.
+ */
+public class EngineStop {
+
+    public static void main(String[] args) {
+
+        System.out.println(EngineConsole.engineStop("E58"));
+    }
+}
---
+++ src/test/java/ServerStop.java
@@ -0,0 +1,18 @@
+import com.seomse.system.server.console.ServerConsole;
+/**
+ * <pre>
+ *  파 일 명 : ServerStop.java
+ *  설    명 : 서버 스탑 테스트
+ *  작 성 자 : macle
+ *  작 성 일 : 2019.11.06
+ *  버    전 : 1.0
+ *  수정이력 :
+ *  기타사항 :
+ * </pre>
+ * @author Copyrights 2019 by ㈜섬세한사람들. All right reserved.
+ */
+public class ServerStop {
+    public static void main(String[] args) {
+        System.out.println((ServerConsole.serverStop("S58")));
+    }
+}
