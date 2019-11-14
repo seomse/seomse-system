b78863919f3cc4022708a8f9d0a0eb6bcd982f03
macle <ysys86a@gmail.com>
2019-11-14 13:54:33 +0900

    bug fix

--- src/main/java/com/seomse/system/engine/EngineConfigData.java
+++ src/main/java/com/seomse/system/engine/EngineConfigData.java
@@ -85,7 +85,7 @@ public class EngineConfigData extends ConfigData implements Synchronizer {
         if(updateTime == 0L){
             engineConfigList = JdbcObjects.getObjList(EngineConfig.class);
         }else{
-            engineConfigList = JdbcObjects.getObjList(EngineConfig.class, "ENGINE_ID='" +engineId +"' UPT_LAST_DT > ?" , PrepareStatements.newTimeMap(updateTime));
+            engineConfigList = JdbcObjects.getObjList(EngineConfig.class, "ENGINE_ID='" +engineId +"' AND UPT_LAST_DT > ?" , PrepareStatements.newTimeMap(updateTime));
         }
 
         if(engineConfigList.size() == 0){
